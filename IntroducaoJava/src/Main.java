7488cb042c0b754081e9e0215e8df0b7729dd29c
ANA HELOISA LEME DE SOUZA GABRIEL <anhelophnrcc@gmail.com>
2026-02-23 10:38:36 -0300

    IMC

---
+++ IntroducaoJava/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+   public static void main(String args []) {
+       System.out.println("Bom dia");
+       //varialvel do tipo String
+       String nome = "HELO";
+       //variavel do tpo int
+       int idade = 42;
+       //variavel do tipo float
+       float peso = 80.0f;
+       float altura = 1.58f;
+       //calcula o IMC
+       float imc = peso / (altura*altura);
+      System.out.println( nome + " seu IMC é " + imc);
+
+
+   }
+}
+
