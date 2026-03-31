2e488373737823932d0e59c7959c78c74e2b0ba8
anhelophnrcc@gmail.com <1090482521007@fatec-srv.local>
2026-03-31 10:19:49 -0300

    exemplo1matrizNotas

--- IntroducaoJava/src/exemplo1matrizNotas.java
+++ IntroducaoJava/src/exemplo1matrizNotas.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class exemplo1matriz {
+public class exemplo1matrizNotas {
+    public static void main(String[] args){
+
+        // declaração da matriz
+        float[][] notas = new float[3][4];
+
+        // leitura da matriz
+        // soma as notas
+        float soma = 0;
+        for(int i=0;i<notas.length;i++){
+            for(int j=0;j<notas[i].length; j++){
+                // gera notas entre 0 e 10
+                notas[i][j] = (float) (Math.random() * 10);
+                soma += notas[i][j];
+                System.out.printf("%.2f   ", notas[i][j]);
+            }
+            System.out.println("");
+        }
+        int qtdeNotas = notas.length * notas[0].length;
+        System.out.printf("\n A média das notas é %.2f \n" , soma / qtdeNotas);
+
+        for (int i = 0; i < notas.length; i++) { // para cada aluno
+            float acum = 0; //zera a soma das notas dos alunos i
+            for (int j = 0; j < notas[i].length; j++) { // para cada prova
+                acum += notas[i][j];
+            }
+            float media = acum / notas[i].length;
+            System.out.println(" A media do aluno " + (i + 1) + " é " + media);
+        }
+
+        for (int j = 0; j < notas[0].length; j++) { //para cada prova
+            float acum = 0;
+            for (int i = 0; i <  notas.length; i++) {
+                acum += notas[i][j];
+            }
+            float media = acum / notas.length;
+            System.out.println(" A média da prova " + (j + 1) + " é " + media);
+        }
+    }
 }
