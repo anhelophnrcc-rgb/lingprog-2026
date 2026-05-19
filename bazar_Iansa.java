238dd15dd9c3aa9da27c3b9f1b4d7468ec3a5df1
anhelophnrcc@gmail.com <1090482521007@fatec-srv.local>
2026-05-19 11:06:17 -0300

    bazar_Iansa

--- bazar_Iansa.java
+++ bazar_Iansa.java
@@ -1,3 +1,26 @@
+import java.sql.*;
+import java.util.Scanner;
+
+
 public class bazar_Iansa {
 
+    public static void criarTabelaUsuarios(Connection conn) throws SQLException{
+
+        String sql = "CREATE TABLE NOT EXISTES usuarios (" +
+                " id SERIAL PRIMARY KEY,"+
+                " nome TEXT NOT NULL," +
+                "email TEXT UNIQUE NOT NULL," +
+                "cpf TEXT UNIQUE NOT NULL,"+
+                "senha TEXT NOT NULL," +
+                "perfil TEXT NOT NULL," +
+
+                // cria objeto de instrução SQL
+            Statement stmt = conn.creatStatement();
+            stmt.execute(sql);
+            stmt.close();
+        System.out.println("Tabela de usuários criada");
+    }
+    public static void cadastrarUsuarios(Connection conn, Scanner sc) throws SQLException{
+
+    }
 }
